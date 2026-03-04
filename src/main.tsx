239b584ef87e4267b4603ae7a3e81ba6cef49662
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2026-03-04 08:44:18 +0200

    update scroll behavior

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import App from "./App.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import "./index.css";
+import ScrollToTop from "./components/partials/ScrollToTop.tsx";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <BrowserRouter>
+          <ScrollToTop />
           <App />
           <Toaster position="top-right" />
         </BrowserRouter>
